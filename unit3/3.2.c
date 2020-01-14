feb7d9447a143716b7aa528affdf9825c1c7e26a
akaQin <qinlong@akaqin.me>
2020-01-14 13:39:10 +0800

    3.2

--- unit3/3.2.c
+++ unit3/3.2.c
@@ -1,7 +1,27 @@
 #include "../apue.3e/include/apue.h"
 #include <fcntl.h>
 
-int main()
+char filebuf1[] = "abcdefghij";
+char filebuf2[] = "ABCDEFGHIJ";
+
+int main(void)
 {
+    int fd;
+    if ((fd = creat("file.hole", FILE_MODE)) < 0) {
+        err_sys("creat error");
+    }
+
+    if (write(fd, filebuf1, 10) != 10) {
+        err_sys("write error");
+    }
+
+    if (lseek(fd, 16384, SEEK_SET) == -1) {
+        err_sys("lseek error");
+    }
+
+    if (write(fd, filebuf2, 10) != 10) {
+        err_sys("buf2 write error");
+    }
+
     exit(0);
 }
