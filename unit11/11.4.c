c4e9100bd324d0219ec11617116ffa3654212b26
akaQin <qinlong@akaqin.me>
2020-04-18 11:36:44 +0800

    thread

--- unit11/11.4.c
+++ unit11/11.4.c
@@ -5,7 +5,7 @@ struct foo {
     int a, b, c, d;
 };
 
-void printfoo(const char *s, const struct foo *fp) {
+void print_foo(const char *s, const struct foo *fp) {
     printf("%s", s);
     printf("  structure at 0x%lx\n", (unsigned long) fp);
     printf("  foo.a = %d\n", fp->a);
@@ -14,35 +14,26 @@ void printfoo(const char *s, const struct foo *fp) {
     printf("  foo.d = %d\n", fp->d);
 }
 
-void *thr_fn1(void *arg) {
+void *thread_func1(void *arg) {
     struct foo foo = {1, 2, 3, 4};
-
-    printfoo("thread 1:\n", &foo);
+    print_foo("thread 1: ", &foo);
     pthread_exit((void *) &foo);
 }
 
-void *thr_fn2(void *arg) {
-    printf("thread 2: ID is %lu\n", (unsigned long) pthread_self());
-    pthread_exit((void *) 0);
-}
 
 int main(void) {
-    int err;
-    pthread_t tid1, tid2;
-    struct foo *fp;
+    pthread_t tid1;
+    struct foo *foo_ptr;
 
-    err = pthread_create(&tid1, NULL, thr_fn1, NULL);
-    if (err != 0)
-        err_exit(err, "can't create thread 1");
-    err = pthread_join(tid1, (void *) &fp);
-    if (err != 0)
-        err_exit(err, "can't join with thread 1");
-    sleep(1);
-    printf("parent starting second thread\n");
-    err = pthread_create(&tid2, NULL, thr_fn2, NULL);
-    if (err != 0)
-        err_exit(err, "can't create thread 2");
-    sleep(1);
-    printfoo("parent:\n", fp);
+    if (pthread_create(&tid1, NULL, thread_func1, NULL) != 0) {
+        err_sys("error");
+    }
+
+    if (pthread_join(tid1, (void*)&foo_ptr) != 0) {
+        err_sys("error");
+    }
+
+    print_foo("main: ", foo_ptr);
+
     exit(0);
 }
