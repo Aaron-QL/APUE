ce5c7b8b55df524d2355c0c40caf144e1964ffe4
akaQin <qinlong@akaqin.me>
2020-04-18 15:07:34 +0800

    thread

--- unit11/11.12.c
+++ unit11/11.12.c
@@ -4,89 +4,83 @@
 #include <pthread.h>
 
 #define NHASH 29
-#define HASH(id) (((unsigned long) id) % NHASH)
-
-struct foo *fh[NHASH];
-pthread_mutex_t hashlock = PTHREAD_MUTEX_INITIALIZER;
+#define HASH(id) (((unsigned long) id) % NNHASH)
 
 struct foo {
-    int f_count; /* protected by hashlock */
-    pthread_mutex_t f_lock;
-    int f_id;
-    struct foo *f_next; /* protected by hashlock */
-    /* ... more stuff here ... */
+    int id;
+    int count;
+    pthread_mutex_t lock;
+    struct foo *next;
 };
 
-struct foo *
-foo_alloc(int id) /* allocate the object */
-{
-    struct foo *fp;
-    int idx;
+struct foo *foo_buffer[NHASH];
 
-    if ((fp = malloc(sizeof(struct foo))) != NULL) {
-        fp->f_count = 1;
-        fp->f_id = id;
-        if (pthread_mutex_init(&fp->f_lock, NULL) != 0) {
-            free(fp);
-            return (NULL);
-        }
-        idx = HASH(id);
-        pthread_mutex_lock(&hashlock);
-        fp->f_next = fh[idx];
-        fh[idx] = fp;
-        pthread_mutex_lock(&fp->f_lock);
-        pthread_mutex_unlock(&hashlock);
-        /* ... continue initialization ... */
-        pthread_mutex_unlock(&fp->f_lock);
+pthread_mutex_t hash_lock = PTHREAD_MUTEX_INITIALIZER;
+
+struct foo *foo_alloc(int id) {
+    int hash_id = HASH(id);
+
+    struct foo *foo_ptr = malloc(sizeof(struct foo));
+    if (foo_ptr == NULL) {
+        return NULL;
     }
-    return (fp);
+
+    foo_ptr->id = id;
+    foo_ptr->count = 0;
+    pthread_mutex_init(&foo_ptr->lock);
+
+    pthread_mutex_lock(&ash_lock);
+    foo_ptr->next = foo_buffer[hash_id];
+    foo_buffer[hash_id] = foo_ptr;
+    pthread_mutex_unlock(&hash_lock);
+
+    return foo_ptr;
 }
 
-void
-foo_hold(struct foo *fp) /* add a reference to the object */
+void foo_hold(struct foo *foo_ptr)
 {
-    pthread_mutex_lock(&hashlock);
-    fp->f_count++;
-    pthread_mutex_unlock(&hashlock);
+    pthread_mutex_lock(&foo_ptr->lock);
+    foo_ptr->count++;
+    pthread_mutex_unlock(&foo_ptr->lock);
 }
 
-struct foo *
-foo_find(int id) /* find an existing object */
+struct foo *foo_find(int id)
 {
-    struct foo *fp;
-
-    pthread_mutex_lock(&hashlock);
-    for (fp = fh[HASH(id)]; fp != NULL; fp = fp->f_next) {
-        if (fp->f_id == id) {
-            fp->f_count++;
+    struct foo *foo_ptr;
+    int hash_id = HASH(id);
+    pthread_mutex_lock(&hash_lock);
+    for (foo_ptr = foo_buffer[hash_id]; foo_ptr != NULL; foo_ptr = foo_ptr->next) {
+        if (foo_ptr->id == id) {
+            foo_hold(foo_ptr);
             break;
         }
     }
-    pthread_mutex_unlock(&hashlock);
-    return (fp);
+    pthread_mutex_unlock(&hash_lock);
+
+    return foo_ptr;
 }
 
-void
-foo_rele(struct foo *fp) /* release a reference to the object */
+void foo_release(struct foo *foo_ptr)
 {
-    struct foo *tfp;
-    int idx;
+    pthread_mutex_lock(&hash_lock);
 
-    pthread_mutex_lock(&hashlock);
-    if (--fp->f_count == 0) { /* last reference, remove from list */
-        idx = HASH(fp->f_id);
-        tfp = fh[idx];
-        if (tfp == fp) {
-            fh[idx] = fp->f_next;
+    if (--foo_ptr->count == 0) {
+        int hash_id = HASH(foo_ptr->id);
+        struct foo *foo_temp = foo_buffer[hash_id];
+        if (foo_temp == foo_ptr) {
+            foo_buffer[hash_id] = foo_ptr->next;
         } else {
-            while (tfp->f_next != fp)
-                tfp = tfp->f_next;
-            tfp->f_next = fp->f_next;
+            while (foo_temp->next != NULL) {
+                if (foo_temp->next == foo_ptr) {
+                    foo_temp->next = foo_ptr->next;
+                    break;
+                }
+            }
         }
-        pthread_mutex_unlock(&hashlock);
-        pthread_mutex_destroy(&fp->f_lock);
-        free(fp);
+        pthread_mutex_unlock(&hash_lock);
+        pthread_mutex_destroy(&foo_ptr->lock);
+        free(foo_ptr);
     } else {
-        pthread_mutex_unlock(&hashlock);
+        pthread_mutex_unlock(&hash_lock);
     }
 }
