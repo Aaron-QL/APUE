08b6063cc7119a287319619a38c3c1736fe655a6
akaQin <qinlong@akaqin.me>
2020-04-20 11:35:21 +0800

    io

--- unit14/14.1.c
+++ unit14/14.1.c
@@ -2,58 +2,65 @@
 #include <errno.h>
 #include <fcntl.h>
 
+//非阻塞I/O
+
 #define BUFFSIZE 4096
 char buf[BUFFSIZE];
 
-//非阻塞I/O
+//设置文件状态
 void set_flag(int fd, int flags)
 {
-    int val;
-    if ((val = fcntl(fd, F_GETFL, 0)) < 0) {
+    int flag_val;
+    if ((flag_val = fcntl(fd, F_GETFL, 0)) < 0) {
         err_sys("fcntl error");
     }
 
     //设置位
-    val |= flags;
-    if (fcntl(fd, F_SETFL, val) < 0) {
+    flag_val |= flags;
+
+    if (fcntl(fd, F_SETFL, flag_val) < 0) {
         err_sys("fcntl error");
     }
 }
 
+//移除文件状态
 void clr_flag(int fd, int flags)
 {
-    int val;
-    if ((val = fcntl(fd, F_GETFL, 0)) < 0) {
+    int flag_val;
+    if ((flag_val = fcntl(fd, F_GETFL, 0)) < 0) {
         err_sys("fcntl error");
     }
 
-    //屏蔽位
-    val &= ~flags;
+    //设置位
+    flag_val &= ~flags;
 
-    if (fcntl(fd, F_SETFL, val) < 0) {
+    if (fcntl(fd, F_SETFL, flag_val) < 0) {
         err_sys("fcntl error");
     }
 }
 
 int main(void)
 {
-    int ntowrite, nwrite;
+    int n_to_write, n_write;
     char *ptr;
 
-    if ((ntowrite = read(STDIN_FILENO, buf, BUFFSIZE)) < 0) {
+    if ((n_to_write = read(STDIN_FILENO, buf, BUFFSIZE)) < 0) {
         err_sys("read error");
     }
-    printf("read %d bytes\n", ntowrite);
+
+    printf("read %d bytes\n", n_to_write);
 
     set_flag(STDOUT_FILENO, O_NONBLOCK);
 
     ptr = buf;
-    while (ntowrite > 0) {
-        nwrite = write(STDOUT_FILENO, ptr, ntowrite);
-        fprintf(stderr, "nwrite = %d, errno = %d\n", nwrite, errno);
-        if (nwrite > 0) {
-            ntowrite -= nwrite;
-            ptr += nwrite;
+    while (n_to_write > 0) {
+        errno = 0;
+        n_write = write(STDOUT_FILENO, ptr, n_to_write);
+        fprintf(stderr, "nwrite = %d, errno = %d\n", n_write, errno);
+
+        if (n_write > 0) {
+            n_to_write -= n_write;
+            ptr += n_write;
         }
     }
 
