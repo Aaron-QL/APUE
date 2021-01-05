28862b9afcbce351b7bbd6150987e7294be5e335
akaQin <qinlong@akaqin.me>
2021-01-05 13:51:32 +0800

    file.hole compare

--- unit3/3.1.c
+++ unit3/3.1.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1) {
+    if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1 ) {
         printf("cannot seek\n");
     } else {
-        puts("seek OK\n");
+        printf("seek OK\n");
     }
 }
--- unit3/3.2.c
+++ unit3/3.2.c
@@ -1,23 +1,60 @@
 #include "../apue.3e/include/apue.h"
 #include <fcntl.h>
 
+// 声明两个字符串
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+
 int main(void)
 {
     int fd;
+    // 创建文件
+    if ((fd = creat("file.nohole", FILE_MODE)) < 0) {
+        err_sys("create error");
+    }
 
-    fd = open("file.hole", O_RDWR | O_TRUNC);
+// 写入第一个字符串
+    if (write(fd, buf1, 10) != 10) {
+        err_sys("buf1 write error");
+    }
 
-    int i = 0;
-    while (i++ < 1000) {
-        if (lseek(fd, 3, SEEK_CUR) == -1) {
-            err_sys("lseek error");
-        }
+// lseek
+//    if (lseek(fd, 16384, SEEK_SET) == -1) {
+//        err_sys("lseek error");
+//    }
 
-        if (write(fd, "abc", 3) != 3) {
-            err_sys("buf2 write error");
-        }
 
+// 写入第二个字符串
+    if (write(fd, buf2, 10) != 10) {
+        err_sys("buf2 write error");
     }
 
     exit(0);
 }
+
+
+
+
+// 退出
+
+
+//int main(void)
+//{
+//    int fd;
+//
+//    fd = open("file.hole", O_RDWR | O_TRUNC);
+//
+//    int i = 0;
+//    while (i++ < 1000) {
+//        if (lseek(fd, 3, SEEK_CUR) == -1) {
+//            err_sys("lseek error");
+//        }
+//
+//        if (write(fd, "abc", 3) != 3) {
+//            err_sys("buf2 write error");
+//        }
+//
+//    }
+//
+//    exit(0);
+//}
